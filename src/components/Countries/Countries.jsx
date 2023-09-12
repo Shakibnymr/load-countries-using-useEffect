dd01a35c3dbfc14a54a3682856ed68e749695c0a
shakibnymr <shakibnymr11@gmail.com>
2023-09-12 06:05:29 +0600

    Conditional CSS and conditional style

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -7,18 +7,32 @@ const Countries = () => {
 
 const [countries,setCountries] = useState([]);
 
+const [visitedCountries,setVisitedCountries] = useState([]);
+
 useEffect(()=>{
 fetch('https://restcountries.com/v3.1/all')
 .then(res => res.json())
 .then(data => setCountries(data))
 },[])
 
+const handleVisitedCountry = country =>{
+    console.log('add this to your visited country')
+    console.log(country)
+}
     return (
         <div >
             <h2>Countries: {countries.length}</h2>
+
+<div>
+    <h2>Visited Countries: </h2>
+    <ul>
+
+    </ul>
+</div>
+
 <div className="country-container">
 {
-    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+    countries.map(country => <Country key={country.cca3} country={country} handleVisitedCountry={handleVisitedCountry}></Country>)
 }
 </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-console.log(country)
+const Country = ({country, handleVisitedCountry}) => {
+// console.log(country)
 const {name, flags,population,area,cca3} = country;
 
 const [visited,setVisited] = useState(false)
@@ -10,17 +10,19 @@ const handleVisited = () =>{
     setVisited(!visited)
 }
 
+console.log(handleVisitedCountry)
     return (
-        <div className='country'>
-            <h2>Name: {name?.common} </h2>
+        <div  className={`country ${visited && 'visited'}`}>
+            <h2 style={{color: visited? 'yellow': 'purple'}}>Name: {name?.common} </h2>
 
 <img src={flags.png} alt="" />
 
 <p>Population: {population}</p>
 <p>Area: {area}</p>
 <p><small>Code: {cca3}</small></p>
+<button>Mark visited</button> <br />
 <button onClick={handleVisited}>{visited? 'visited' : 'going'}</button>
-{visited && 'I have visited this country'}
+{visited && '  I have visited this country'}
         </div>
     );
 };
